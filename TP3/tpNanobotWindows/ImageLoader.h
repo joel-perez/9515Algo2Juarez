e13f168062bda0f472e1d09ac3300f966792db47
Joel Perez <joel_perez84@hotmail.com>
2019-11-12 20:01:38 -0300

    modificaciones en el proyecto windows para que funcione en linux tambien... ya esta de mas este proyecto, jaja

--- TP3/tpNanobotWindows/ImageLoader.h
+++ TP3/tpNanobotWindows/ImageLoader.h
@@ -5,7 +5,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include "Textura.h"
-#include "conversiones.h"
 
 using namespace std;
 
--- TP3/tpNanobotWindows/conversiones.cpp
+++
@@ -1,9 +0,0 @@
-#include "conversiones.h"
-
-using namespace std;
-
-string to_string(int number) {
-    ostringstream oss;
-    oss << number;
-    return oss.str();
-}
