6684a9e53284509f1e02988c18c19625d82a235f
Pablo <pablobrocca1999@gmail.com>
2019-11-12 21:15:15 -0300

    Modificado main.cpp para que cree una lista dinamica

--- ordenamientos/main.cpp
+++ ordenamientos/main.cpp
@@ -7,21 +7,21 @@ using namespace std;
 
 int main()
 {
-    Lista l1;
+    Lista* l1 = new Lista;
 
     for (int i=1; i<=MAX; i++) {
         int nuevo = rand() % 127 + 1;
-        l1.agregar(nuevo);
+        l1->agregar(nuevo);
     }
 
-    Nodo *primero = l1.obtener_primero();
+    Nodo *primero = l1->obtener_primero();
 
     cout << "Lista sin ordenar: " << endl;
-    l1.mostrar();
+    l1->mostrar();
 
-    l1.mergesort(&primero);
+    l1->mergesort(&primero);
 
     cout << "Lista ordenada: " << endl;
-    l1.mostrar();
+    l1->mostrar();
 
 }
