99ef7906a29ca749980b6d79a1ef6cb47c8405b6
juankbn <karabin.juan@gmail.com>
2019-11-20 13:52:23 -0300

    Subo QuickSort

---
+++ ordenamientos/Quicksort/quicksort.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include "quicksort.h"
+
+using namespace std;
+
+// Funcion para intercambiar dos elementos
+void intercambiar(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+/*Función que toma el último elemento como pivote, situa
+el elemento pivote en su posición correcta en el arreglo
+ordenado y coloca todos los elementos más pequeños que el
+a su izquierda, y todos los elementos mayores a su derecha
+*/
+int dividir (int arreglo[], int primero, int ultimo)
+{
+    int pivote = arreglo[ultimo]; // pivote
+    int i = (primero - 1); // indice del elemento menor
+
+    for (int j = primero; j <= ultimo - 1; j++)
+    {
+        // Si el elemento es menor que el pivotee
+        if (arreglo[j] < pivote)
+        {
+            i++; // incrementa el indice del elemento menor
+            intercambiar(&arreglo[i], &arreglo[j]);
+        }
+    }
+    intercambiar(&arreglo[i + 1], &arreglo[ultimo]);
+    return (i + 1);
+}
+
+/* FUNCION PRINCIPAL QUE IMPLEMENTA QuickSort
+arreglo[] --> arreglo a ser ordenado,
+primero --> Indice del comienzo
+ultimo --> Indice del final */
+void quickSort(int arreglo[], int primero, int ultimo)
+{
+    if (primero < ultimo)
+    {
+        /* pi divide el indice, arreglo[p] ahora esta
+        en el lugar correcto */
+        int pi = dividir(arreglo, primero, ultimo);
+
+        // Separar elementos antes separados
+        // dividir y despues dividir
+        quickSort(arreglo, primero, pi - 1);
+        quickSort(arreglo, pi + 1, ultimo);
+    }
+}
+
+/* Funcion para imprimir el arreglo */
+void imprimirarreglo(int arreglo[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arreglo[i] << " ";
+    cout << endl;
+}
---
+++ ordenamientos/Quicksort/quicksort.h
@@ -0,0 +1,9 @@
+#ifndef QUICKSORT_H_INCLUDED
+#define QUICKSORT_H_INCLUDED
+
+void intercambiar(int* a, int* b);
+int dividir (int arreglo[], int primero, int ultimo);
+void quickSort(int arreglo[], int primero, int ultimo);
+void imprimirarreglo(int arreglo[], int size);
+
+#endif // QUICKSORT_H_INCLUDED
