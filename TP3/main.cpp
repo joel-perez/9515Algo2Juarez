d864c8f4a04ae35c238c3ec490ec0ed70647cdd5
Joel Perez <joperezr@gmail.com>
2019-11-21 00:27:51 -0300

    Pruebo mis credenciales en el GIT.

--- TP3/main.cpp
+++ TP3/main.cpp
@@ -5,7 +5,6 @@ using namespace std;
 Juego* juego = 0;
 
 int main() {
-    cout << "TP3 - Nanobot - Grupo: 'Sobrecargados'" << endl;
     juego = new Juego();
     juego->iniciar("TP3 - Nanobot - Grupo: 'Sobrecargados'", 100, 100, 0);
     juego->correr();
