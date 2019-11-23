23e2bed06a182557d26bb51212f70375fe1631de
juankbn <karabin.juan@gmail.com>
2019-11-23 16:40:45 -0300

    modifico radix

--- ordenamientos/Radix_Sort/main.cpp
+++ ordenamientos/Radix_Sort/main.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 using namespace std;
 #include <math.h>
-#define NUMELTS 20
 
 void radixsort(int x[], int n)
 {
@@ -9,25 +8,25 @@ void radixsort(int x[], int n)
 
     struct {
         int info;
-        int next;
-    } node[NUMELTS];
+        int siguiente;
+    } node[10];
 
-    int exp, first, i, j, k, p, q, y;
+    int exp, primero, i, j, k, p, q, y;
 
   /* Inicializar una lista vinculada */
     for (i = 0; i < n-1; i++)
     {
         node[i].info = x[i];
-        node[i].next = i+1;
+        node[i].siguiente = i+1;
     } /* fin del for */
 
     node[n-1].info = x[n-1];
-    node[n-1].next = -1;
-    first = 0; /* first es la cabeza de la lista vinculada */
+    node[n-1].siguiente = -1;
+    primero = 0; /* primero es la cabeza de la lista vinculada */
 
     for (k = 1; k < 5; k++)
     {
-    /* Suponer que tenemos números de cuatro dígitos */
+    /* Suponer que tenemos números de cuatro dígitos como maximo */
         for (i = 0; i < 10; i++)
         {
         /*Inicializar colas */
@@ -36,29 +35,30 @@ void radixsort(int x[], int n)
         } /*fin del for */
 
         /* Procesar cada elemento en la lista */
-        while (first != -1)
+        while (primero != -1)
         {
-            p = first;
-            first = node[first].next;
+            p = primero;
+            primero = node[primero].siguiente;
             y = node[p].info;
-            /* Extraer el kâsimo dÁgito */
+            /* Extraer el k-asimo digito */
             exp = pow(10, k-1); /* elevar 10 a la (k-1)ésima potencia */
             j = (y/exp) % 10;
-            /* Insertar y en queue[j] */
+            /* Insertar en cola[j] */
             q = posterior[j];
             if (q == -1)
                 anterior[j] = p;
             else
-                node[q].next = p;
+                node[q].siguiente = p;
             posterior[j] = p;
         } /*fin del while */
 
         /* En este punto, cada registro está en su cola basándose en el dígito k
-           Ahora formar una lista única de todos los elementos de la cola.
-           Encontrar el primer elemento. */
+           Ahora se forma una lista única de todos los elementos de la cola.
+           Se encuetra el primer elemento. */
         for (j = 0; j < 10 && anterior[j] == -1; j++);
             ;
-        first = anterior[j];
+        primero = anterior[j];
+
 
         /* Vincular las colas restantes */
         while (j <= 9)
@@ -69,18 +69,18 @@ void radixsort(int x[], int n)
             if (i <= 9)
             {
                 p = i;
-                node[posterior[j]].next = anterior[i];
+                node[posterior[j]].siguiente = anterior[i];
             } /* fin del if */
             j = i;
         } /* fin del while */
-        node[posterior[p]].next = -1;
+        node[posterior[p]].siguiente = -1;
     } /* fin del for */
 
     /* Copiar de regreso al archivo original */
     for (i = 0; i < n; i++)
     {
-            x[i] = node[first].info;
-            first = node[first].next;
+            x[i] = node[primero].info;
+            primero = node[primero].siguiente;
     } /*fin del for */
 } /* fin de radixsort*/
 
@@ -93,15 +93,15 @@ void imprimirarreglo(int arreglo[], int size)
     cout << endl;
 }
 
-int main(void)
+int main()
 {
 
-    int x[] = {987, 943, 854, 100, 123, 534, 578};
+    int x[] = {1987, 943, 854, 1010, 123, 5341, 578};
     int n = sizeof(x) / sizeof(x[0]);
     cout<<"Arreglo sin ordenar: \n";
     imprimirarreglo(x,n);
     radixsort (x, n);
-    cout<<"Arreglo ordenado: \n";
+    cout<<"\nArreglo ordenado: \n";
     imprimirarreglo(x,n);
 
     return 0;
