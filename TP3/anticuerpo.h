85590262821030f8aaa60babcee8e2166799148f
cndeserrano <56178586+cndeserrano@users.noreply.github.com>
2019-11-24 18:35:16 -0300

    Add files via upload
    Estan agregados los metodos para que hayan posiciones aleatorias en anticuerpo, habria que agregarlas en entorno

--- TP3/anticuerpo.h
+++ TP3/anticuerpo.h
@@ -2,6 +2,8 @@
 #define ANTICUERPO_H
 
 #include <string>
+#include <stdlib.h>
+#include <time.h>
 #include "elemento.h"
 
 class Anticuerpo : public Elemento {
@@ -10,6 +12,8 @@ class Anticuerpo : public Elemento {
         Anticuerpo(std::string tipo, float posicion_x, float posicion_y);
         std::string resumen_datos();
         std::string detalles_datos();
+        float posicion_aleatoria_x ();
+        float posicion_aleatoria_y ();
 };
 
 #endif // ANTICUERPO_H
