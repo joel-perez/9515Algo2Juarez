44d2ddac19ccd965a5e0905c443b4fc2f36a9dd8
juankbn <karabin.juan@gmail.com>
2019-11-27 17:38:45 -0300

    Agrego comentarios a Quicksort para ir explicando

--- ordenamientos/Quicksort/quicksort.cpp
+++ ordenamientos/Quicksort/quicksort.cpp
@@ -1,57 +1,15 @@
 #include <iostream>
 using namespace std;
 
-
-/* FUNCION PRINCIPAL QUE IMPLEMENTA QuickSort*/
-void quicksort(int arreglo[],int primero, int ultimo )
-{
-    int i, j, x, aux;
-    i = primero;
-    j = ultimo;
-    x = arreglo[ (primero + ultimo) /2 ]; //PIVOTE
-    do
-    {
-        while( (arreglo[i] < x) && (j <= ultimo) )
-        {
-            i++;
-        }
-
-        while( (x < arreglo[j]) && (j > primero) )
-        {
-            j--;
-        }
-
-        if( i <= j )
-        {
-            aux = arreglo[i];
-            arreglo[i] = arreglo[j];
-            arreglo[j] = aux;
-            i++;
-            j--;
-        }
-
-    }
-    while( i <= j );
-
-    if( primero < j )
-        quicksort( arreglo, primero, j );
-    if( i < ultimo )
-        quicksort( arreglo, i, ultimo );
-}
-
 //FUNCION PARA IMPRIMIR EL ARREGLO
-void imprimirarreglo(int arreglo[], int size)
-{
-    int i;
-    for (i = 0; i < size; i++)
-        cout << arreglo[i] << " ";
-    cout << endl;
-}
+void imprimirarreglo(int [], int);
 
-// MAIN
+//FUNCION PRINCIPAL QUE IMPLEMENTA QuickSort
+void quicksort(int [],int, int);
+
 int main()
 {
-    int arreglo[] = {10, 7, 8, 9, 1, 5};
+    int arreglo[] = {11,8,2,6,9,1,5,4};
     int n = sizeof(arreglo) / sizeof(arreglo[0]);
     cout << "arreglo sin ordenar: \n";
     imprimirarreglo(arreglo, n);
@@ -59,5 +17,54 @@ int main()
     cout << "arreglo ordenado: \n";
     imprimirarreglo(arreglo, n);
     return 0;
+}
+
+
+void quicksort(int arreglo[],int primero, int ultimo )
+{
+    int i, j, central, pivote;
+    i = primero;
+    j = ultimo;
+    central = ((primero + ultimo) /2);
+    pivote = arreglo[central];
+    do
+    {
+        //SEPARA EN DOS EL ARRAY
+        while (arreglo[i] < pivote) //VALORES MENORES QUE EL PIVOTE
+        {
+            i++;
+        }
+
+        while (pivote < arreglo[j]) //VALORES MAYORES QUE EL PIVOTE
+        {
+            j--;
+        }
+
+        if( i <= j )
+        {
+            int aux;
+            //INTERCAMBIO DE VALORES
+            aux = arreglo[i];
+            arreglo[i] = arreglo[j];
+            arreglo[j] = aux;
+            i++;
+            j--;
+        }
+
+    } while ( i <= j );
+
+    if (primero < j )
+        quicksort( arreglo, primero, j ); //LA PRIMERA POSICION ASUME EL VALOR DE primero,  Y LA SEGUNDA DE j
+    if (i < ultimo)
+        quicksort( arreglo, i, ultimo ); //LA PRIMERA POSICION ES i, Y LA ULTIMA ultimo
+}
+
+
+void imprimirarreglo(int arreglo[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arreglo[i] << " ";
+    cout << endl;
 }
 
