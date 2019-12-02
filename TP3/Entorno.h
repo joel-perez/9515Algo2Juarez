10c55649f9c81a6188accde1582c9d1142a9ceca
Joel Perez <joperezr@gmail.com>
2019-12-02 10:15:23 -0300

    Hago merge con los cambios de Cande.

--- TP3/Entorno.h
+++ TP3/Entorno.h
@@ -7,7 +7,6 @@
 #include "SDL2/SDL_image.h"
 #include "ImageLoader.h"
 #include "tejido.h"
-#include "tipodosis.h"
 #include "imagenes.h"
 #include "dosis.h"
 
@@ -26,33 +25,76 @@ class Entorno
     Tejido* tejido;
 
     public:
+        //PRE:-
+        //POST: genera un entorno
         Entorno();
         ~Entorno();
+        //PRE:-
+        //POST:
         void cargar_texturas();
+        //PRE:
+        //POST:
         void renderizar(imagenes imagen, float x, float y);
+        //PRE:
+        //POST:
         bool iniciar(const char *titulo, int xPos, int yPos, int flags);
+        //PRE:
+        //POST:
         void renderizar_todo();
+        //PRE:
+        //POST:
         void limpiar();
+        //PRE:
+        //POST:
         void explotarDosis(dosis dosis);
+        //PRE:
+        //POST:
         bool dosisAExplotando();
+        //PRE:
+        //POST:
         bool dosisBExplotando();
+        //PRE:
+        //POST:
         void dibujar_lineas_entre_celulas();
+        //PRE:
+        //POST:
         void dibujar_celulas();
+        //PRE:
+        //POST:
         void dibujar_anticuerpos();
+        //PRE:
+        //POST:
         void dibujar_dosis();
+        //PRE:
+        //POST:
         void mutar_celulas();
+        //PRE:
+        //POST:
         void generar_anticuerpo();
+        //PRE:
+        //POST:
         void detector_colisiones();
-        void inyectar_dosis(TipoDosis tipo_dosis);
+        //PRE:
+        //POST:
         float obtener_nanobot_pos_x();
+        //PRE:
+        //POST:
         float obtener_nanobot_pos_y();
+        //PRE:
+        //POST:
         void cambiar_nanobot_pos_x(int x);
+        //PRE:
+        //POST:
         void cambiar_nanobot_pos_y(int y);
+        //PRE:
+        //POST:
         unsigned int obtener_cantidad_total_celulas();
+        //PRE:
+        //POST:
         unsigned int obtener_cantidad_celulas(string tipo_celula);
+        //PRE:
+        //POST:
         bool hay_colision(float pos_x1, float pos_y1, float pos_x2, float pos_y2, int ancho_objeto1, int ancho_objeto2);
-
-        bool inyectando_dosis;
 };
 
 #endif // ENTORNO_H
--- TP3/elemento.h
+++ TP3/elemento.h
@@ -11,12 +11,26 @@ class Elemento {
         float posicion_y;
 
     public:
+        //PRE:-
+        //POST: genera el elemento
         Elemento();
+        //PRE:-
+        //POST: genera el elemento
         Elemento(std::string tipo, float posicion_x, float posicion_y);
+        //PRE: -
+        //POST: retorna en un string los datos del elemento
         virtual std::string resumen_datos();
+        //PRE: -
+        //POST: retorna en un string los detalle de los datos del elemento
         virtual std::string detalles_datos();
+        //PRE: -
+        //POST: retorna la posicion x del elemento
         virtual float obtener_posicion_x();
+        //PRE: -
+        //POST: retorna la posicion y del elemento
         virtual float obtener_posicion_y();
+        //PRE: -
+        //POST: retorna en un string el tipo de elemento
         virtual std::string obtener_tipo();
         virtual ~Elemento();
 };
